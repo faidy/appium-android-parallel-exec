60cd4c648e9f388e5a59dae55cdab8e24ada2a06
Ahmed Faidy <ah.faidy@gmail.com>
2015-05-19 14:43:35 +0300

    Adding a longer scenario-5 notes

--- cucumber/features/addnote.feature
+++ cucumber/features/addnote.feature
@@ -4,3 +4,7 @@ Add note
 Scenario: Note is added successfully
 	Given I Add a new note "This is my note"
 	Then the note "This is my note" is added successfully
+
+Scenario: Adding five notes successfully
+	Given I add 5 notes
+	Then the 5 notes are added successfully
--- cucumber/features/step_definitions/addsteps.rb
+++ cucumber/features/step_definitions/addsteps.rb
@@ -1,10 +1,25 @@
 Given(/^I Add a new note "(.*?)"$/) do |arg1|
-find_element(:id,'menu_add').click
-find_element(:id,'note').send_keys arg1
-find_element(:id,'menu_save').click
+	find_element(:id,'menu_add').click
+	find_element(:id,'note').send_keys arg1
+	find_element(:id,'menu_save').click
+end
+
+Given(/^I add (\d+) notes$/) do |arg1|
+	num=arg1.to_i
+	i=1
+	while i <= num do
+		find_element(:id,'menu_add').click
+		find_element(:id,'note').send_keys "Note "+i.to_s
+		find_element(:id,'menu_save').click
+		i+=1
+	end
+end
+
+Then(/^the (\d+) notes are added successfully$/) do |arg1|
+	find_elements(:id,'text1').length.should eq arg1.to_i
 end
 
 Then(/^the note "(.*?)" is added successfully$/) do |arg1|
-find_elements(:id,'text1').length.should eq 1
-texts.last.text.should eq arg1
+	find_elements(:id,'text1').length.should eq 1
+	texts.last.text.should eq arg1
 end
